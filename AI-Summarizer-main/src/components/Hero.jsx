12e090ca33c625681a91bf4ac74a07d76505f5a5
in-c0 <wldud5192@gmail.com>
2024-09-04 21:02:44 +1000

    Initial commit

---
+++ AI-Summarizer-main/src/components/Hero.jsx
@@ -0,0 +1,32 @@
+import { logo } from "../assets";
+
+const Hero = () => {
+    return (
+        <header className='w-full flex justify-center items-center flex-col'>
+            <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+                <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+
+                <button
+                    type='button'
+                    onClick={() =>
+                        window.open("https://github.com/in-c0/GRAKKEN-Github-Repo-Analyser", "_blank")
+                    }
+                    className='black_btn'
+                >
+                    GitHub
+                </button>
+            </nav>
+
+            <h1 className='head_text'>
+                Simplify Git Repository with <br className='max-md:hidden' />
+                <span className='orange_gradient '>OpenAI GPT-4</span>
+            </h1>
+            <h2 className='desc'>
+                Simplify your project with Grakken, an open-source git repository analyzer
+                that helps you understand repositories right away.
+            </h2>
+        </header>
+    );
+};
+
+export default Hero;
