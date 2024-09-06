ce3144bf2454f77b64736e23b1a2db9020cfecf7
in-c0 <wldud5192@gmail.com>
2024-09-06 16:50:15 +1000

    attempt to fix preline UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import Hero from './components/Hero';
 import Demo from './components/Demo';
+import "preline/preline";
 import './App.css';
 
 const App = () => {
+    
+    // reinitialize UI components (HSStaticMethods) on route change / page reload
+    const location = useLocation();
+    useEffect(() => {
+        if (window.HSStaticMethods) {
+        window.HSStaticMethods.autoInit();
+        }
+    }, [location.pathname]);
+
+    
     return (
         <main>
             <div className="main">
