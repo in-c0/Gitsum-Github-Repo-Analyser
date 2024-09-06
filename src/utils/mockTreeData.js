1fe2f7da6519cac741087cf4dc28c1e68924f46f
in-c0 <wldud5192@gmail.com>
2024-09-06 18:41:03 +1000

    preline UI corrected treeview format. still offline

--- src/utils/mockTreeData.js
+++ src/utils/mockTreeData.js
@@ -2,22 +2,24 @@
 export const mockTreeData = [
     {
       name: 'root',
+      type: 'dir',
       children: [
         {
           name: 'src',
+          type: 'dir',
           children: [
-            { name: 'components', children: [{ name: 'Demo.jsx' }, { name: 'Hero.jsx' }] },
-            { name: 'services', children: [{ name: 'article.js' }] },
-            { name: 'utils', children: [{ name: 'index.js' }] },
+            { name: 'components', type: 'dir', children: [{ name: 'Demo.jsx', type: 'file' }, { name: 'Hero.jsx', type: 'file' }] },
+            { name: 'services', type: 'dir', children: [{ name: 'article.js', type: 'file' }] },
+            { name: 'utils', type: 'dir', children: [{ name: 'index.js', type: 'file' }] },
           ],
         },
         {
           name: 'public',
-          children: [{ name: 'favicon.ico' }, { name: 'index.html' }],
+          type: 'dir',
+          children: [{ name: 'favicon.ico', type: 'file' }, { name: 'index.html', type: 'file' }],
         },
-        { name: 'package.json' },
-        { name: 'README.md' },
+        { name: 'package.json', type: 'file' },
+        { name: 'README.md', type: 'file' },
       ],
     },
   ];
-  
